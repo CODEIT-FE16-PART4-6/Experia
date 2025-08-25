88a02f67ec12e3cc5b466daa793db0e35e9bf0a8
Apologle <otl5994@naver.com>
2025-08-25 22:10:52 +0900

    fix: api 오류(401) 수정 **.env 4팀으로 변경함. 차후에 6팀으로 돌려놓기**

--- src/utils/api-public/index.ts
+++ src/utils/api-public/index.ts
@@ -1,6 +1,7 @@
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 const TEAM = process.env.NEXT_PUBLIC_TEAM;
 
-export const REQUEST_URL = `${BACKEND_URL}/${TEAM}`
+export const REQUEST_URL = `${BACKEND_URL}/${TEAM}`;
 
-export const tokenTmp: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQ3MCwidGVhbUlkIjoiMTYtNCIsImlhdCI6MTc1NjAzMjQ4NywiZXhwIjoxNzU3MjQyMDg3LCJpc3MiOiJzcC1nbG9iYWxub21hZCJ9.QkDK6yd2_8T4wwqPGXGdjNi-RjVTZTm2nPy_yjoFCOw'
+export const tokenTmp: string =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQ1NSwidGVhbUlkIjoiMTYtNiIsImlhdCI6MTc1NjEyNDY1MywiZXhwIjoxNzU2MTI2NDUzLCJpc3MiOiJzcC1nbG9iYWxub21hZCJ9.WR4hz2GjAU8-FrFb5sAEhU744qSnSQW6z_4x0XZ2Lm8';
