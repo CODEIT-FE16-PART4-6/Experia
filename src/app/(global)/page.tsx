c989e32f994b517f6247cacaee524a5c9f882e03
htak0601 <taklee2@naver.com>
2025-08-26 18:12:34 +0900

    feat: 버튼 스타일/외부 상태 disabled

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -1,30 +1,39 @@
 'use client';
 
+import Button from '@/components/Button';
 import { StarRating } from '@/components/StarRating';
-import ButtonField from '@/components/Button';
 import { useState } from 'react';
 
 export default function Home() {
   const [rating, setRating] = useState(0);
+  // 상태에 따른 버튼 활성화/비활성화
+  const isDisabled = true;
 
   return (
     <main>
+      <div className="w-80">
+        <h2 className="text-xl font-bold mb-2">체험 상세 테스트</h2>
+        <Button variant="POSITIVE" size="lg" disabled={isDisabled}>
+          예약하기
+        </Button>
+      </div>
+
       <section>
         <h2 className="text-xl font-bold mb-2">버튼 테스트</h2>
         <div className="w-1/5 flex flex-col gap-2 mb-5">
-          <ButtonField variant='primary' size='lg'>로그인 하기</ButtonField>
-          <ButtonField variant='secondary' size='lg'>로그인 하기</ButtonField>
-          <ButtonField variant="danger" size="lg" disabled>신청 불가</ButtonField>
+          <Button variant='DEFAULT' size='lg'>로그인 하기</Button>
+          <Button variant='POSITIVE' size='lg' >로그인 하기</Button>
+          <Button variant="NEGATIVE" size="lg" disabled>신청 불가</Button>
         </div>
-        <div className="w-1/8 flex flex-col gap-2 mb-5">
-          <ButtonField variant='primary' size='md'>로그인 하기</ButtonField>
-          <ButtonField variant='secondary' size='md'>로그인 하기</ButtonField>
-          <ButtonField variant="danger" size="md" disabled>신청 불가</ButtonField>
+        <div className="w-1/6 flex flex-col gap-2 mb-5">
+          <Button variant='DEFAULT' size='md'>로그인 하기</Button>
+          <Button variant='POSITIVE' size='md'>로그인 하기</Button>
+          <Button variant="NEGATIVE" size="md" disabled>신청 불가</Button>
         </div>
-        <div className="w-1/14 flex flex-col gap-2 mb-5">
-          <ButtonField variant='primary' size='sm'>로그인 하기</ButtonField>
-          <ButtonField variant='secondary' size='sm'>로그인 하기</ButtonField>
-          <ButtonField variant="danger" size="sm" disabled>신청 불가</ButtonField>
+        <div className="w-1/16 flex flex-col gap-2 mb-5">
+          <Button variant='DEFAULT' size='sm'>로그인 하기</Button>
+          <Button variant='POSITIVE' size='sm'>로그인 하기</Button>
+          <Button variant="NEGATIVE" size="sm" disabled>신청 불가</Button>
         </div>
       </section>
 
