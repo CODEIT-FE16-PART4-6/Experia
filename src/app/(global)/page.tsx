12f18f3a52095fa7e5adde79498aecaee8e5fa0d
asksa1256@gmail.com <asksa1256@gmail.com>
2025-08-27 19:59:57 +0900

    [P6-95] refactor: 메인 체험 리스트 렌더링 방식 SSG로 설정

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -9,6 +9,7 @@ const fetchActivities = async ({ page, size }: { page: number; size: number }) =
   const data = await fetchServerData<Activities>({
     path: '/activities',
     query: { method: 'cursor', page, size },
+    renderType: 'ssg',
   });
 
   return data;
