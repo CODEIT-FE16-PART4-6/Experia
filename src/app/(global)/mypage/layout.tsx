7f34c3f5fc4071d47be2a0ba8d5d8aaaf5c6d50f
김이서 <12305025k@gmail.com>
2025-08-28 14:17:41 +0900

    [P6-97] fix: 반응형 적용

--- src/app/(global)/mypage/layout.tsx
+++ src/app/(global)/mypage/layout.tsx
@@ -1,15 +1,19 @@
 import Snb from '@/components/layout/Snb';
+import { BREAKPOINTS } from '@/constants';
+
 interface MyPageLayoutProps {
   children: React.ReactNode;
 }
 
 export default function MyPageLayout({ children }: MyPageLayoutProps) {
   return (
-    <div className='flex min-h-screen flex-shrink-0 md:ml-[24px] lg:ml-[360px]'>
-      <aside>
-        <Snb />
-      </aside>
-      <section className='flex-1 p-4'>{children}</section>
+    <div>
+      <div className='mt-[72px] flex min-h-screen w-[344px] flex-col md:w-[696px] md:flex-row lg:mx-auto lg:w-[1200px]'>
+        <aside>
+          <Snb />
+        </aside>
+        <section className='flex-1 p-4'>{children}</section>
+      </div>
     </div>
   );
 }
--- src/components/layout/Snb.tsx
+++ src/components/layout/Snb.tsx
@@ -6,28 +6,27 @@ import Image from 'next/image';
 const Snb = () => {
   const windowSize = useWindowWidth();
 
-  if (windowSize === undefined) {
-    return null; // 또는 로딩 상태 표시
+  if (!windowSize) {
+    return null;
   }
-
-  const widthClass = windowSize >= 1024 ? 'w-[384px]' : windowSize >= 600 ? 'w-[251px]' : 'hidden'; // 모바일일 때 숨기기
-  if (widthClass === 'hidden') return null; // 모바일에서는 아예 숨기기
 
   return (
     <nav
-      className={`mt-[72px] h-[432px] bg-gray-100 font-bold text-gray-700 ${widthClass} items-center rounded-xl border border-gray-400 shadow-2xl`}
+      className={`m-1 h-[432px] items-center rounded-xl border border-gray-400 bg-gray-100 font-bold text-gray-700 shadow-2xl sm:w-[344px] md:w-[251px] lg:w-[384px]`}
     >
       <div className='mx-auto mt-6 mb-6 flex items-center justify-center'>
-        <Image src='/images/img_user_mock.png' alt='유저 목이미지' width={160} height={160} />
-        <button className='absolute'>
-          <Image
-            src='/icons/ic_edit.svg'
-            alt='유저 사진 수정 아이콘'
-            width={44}
-            height={44}
-            className='relative bottom-[-43px] left-[44px]'
-          />
-        </button>
+        <div className='relative'>
+          <Image src='/images/img_user_mock.png' alt='유저 목이미지' width={160} height={160} />
+          <button className='absolute right-[15px] bottom-[10px]'>
+            <Image
+              src='/icons/ic_edit.svg'
+              alt='유저 사진 수정 버튼'
+              width={44}
+              height={44}
+              className=''
+            />
+          </button>
+        </div>
       </div>
       <ul className='mx-6'>
         <li className='group hover:text-nomad-black'>
