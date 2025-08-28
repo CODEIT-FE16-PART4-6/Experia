c8903f6ea8a845102e63aba88c3cccfb683e445a
leesangdal <asksa1256@gmail.com>
2025-08-29 00:29:37 +0900

    feat: cn 유틸 함수 생성 및Button에 적용 (clsx+twMerge로 외부 className 적용 우선순위 충돌 문제 해결)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 // Headless UI Button alias
 import { Button as HeadlessButton } from '@headlessui/react';
-import clsx from 'clsx';
+import { cn } from '@/utils/cn';
 
 // 버튼 스타일 타입
 type ButtonStyle = 'DEFAULT' | 'POSITIVE' | 'NEGATIVE';
@@ -16,16 +16,24 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string; // 추가 클래스 적용!
 }
 
-const Button = ({ variant = 'DEFAULT', size = 'md', disabled = false, className, children, ...rest }: ButtonProps) => {
+const Button = ({
+  variant = 'DEFAULT',
+  size = 'md',
+  disabled = false,
+  className,
+  children,
+  ...rest
+}: ButtonProps) => {
   // 기본 스타일
-  const baseStyle = 'w-full rounded-md border font-semibold transition-all flex justify-center items-center'
+  const baseStyle =
+    'w-full rounded-md border font-semibold transition-all flex justify-center items-center';
 
   // variant 클래스
   const variantClasses: Record<ButtonStyle, string> = {
     DEFAULT: 'bg-white hover:bg-nomad-black hover:text-white disabled:bg-gray-600',
     POSITIVE: 'bg-nomad-black text-white disabled:bg-gray-600',
     NEGATIVE: 'bg-gray-600 text-white disabled:bg-gray-600',
-  }
+  };
 
   // size 클래스
   const sizeClasses: Record<ButtonSize, string> = {
@@ -35,12 +43,13 @@ const Button = ({ variant = 'DEFAULT', size = 'md', disabled = false, className,
   };
 
   return (
-    <HeadlessButton className={clsx(baseStyle, variantClasses[variant], sizeClasses[size], className)}
+    <HeadlessButton
+      className={cn(baseStyle, variantClasses[variant], sizeClasses[size], className)}
       disabled={disabled}
       {...rest}
     >
       {children}
-    </HeadlessButton >
+    </HeadlessButton>
   );
 };
 
