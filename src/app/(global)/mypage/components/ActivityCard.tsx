52b639d5317928533732f8cc2503d4e04093f520
김이서 <12305025k@gmail.com>
2025-08-28 17:58:08 +0900

    [P6-104] fix: 이미지 비율 반응형 적용

--- src/app/(global)/mypage/components/ActivityCard.tsx
+++ src/app/(global)/mypage/components/ActivityCard.tsx
@@ -28,15 +28,14 @@ interface ActivityCardProps {
  */
 const ActivityCard = ({ reservation }: ActivityCardProps) => {
   return (
-    <div className='h-[204px]rounded-lg mb-6 ml-6 flex w-[792px] overflow-hidden rounded-2xl shadow-2xl'>
-      <div>
+    <div className='flex h-[128px] w-full overflow-hidden rounded-2xl shadow-2xl md:h-[156px] lg:h-[204px]'>
+      <div className='relative h-[128px] w-[128px] md:h-[156px] md:w-[156px] lg:h-[204px] lg:w-[204px]'>
         <figure>
           <Image
             src={reservation.activity.bannerImageUrl}
             alt='액티비티 배너 사진'
-            width={204}
-            height={204}
-            className='h-[204px] w-[204px] object-cover'
+            fill
+            className='object-cover'
           />
         </figure>
       </div>
