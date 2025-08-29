0b5b74d3ab8a247b9043e53a74144bda613c4089
leesangdal <asksa1256@gmail.com>
2025-08-29 13:04:52 +0900

    fix: 마이페이지 레이아웃 min-height:100vh 추가

--- src/app/(global)/mypage/layout.tsx
+++ src/app/(global)/mypage/layout.tsx
@@ -6,7 +6,7 @@ interface MyPageLayoutProps {
 
 export default function MyPageLayout({ children }: MyPageLayoutProps) {
   return (
-    <div className='bg-gray-100'>
+    <div className='min-h-screen bg-gray-100'>
       <div className='flex flex-col gap-4 px-6 pt-[72px] md:flex-row lg:mx-auto lg:w-[1200px] lg:gap-6 lg:px-0'>
         <aside className='h-full shrink-0 items-center rounded-xl border border-gray-400 bg-gray-100 p-6 font-bold text-gray-700 shadow-2xl sm:mx-auto sm:w-[344px] md:mx-0 md:w-[251px] lg:w-[384px]'>
           <Snb />
