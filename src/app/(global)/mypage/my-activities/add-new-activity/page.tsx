a5b2ada69645982116b99e0f62671b0e72807c5a
asksa1256@gmail.com <asksa1256@gmail.com>
2025-08-29 17:01:58 +0900

    [P6-96] fix: 체험 등록 페이지 - '돌아가기' 버튼 경로 수정

--- src/app/(global)/mypage/my-activities/add-new-activity/page.tsx
+++ src/app/(global)/mypage/my-activities/add-new-activity/page.tsx
@@ -8,7 +8,7 @@ const AddNewActivityPage = () => {
   return (
     <section>
       <Link
-        href='/mypage/myActivities'
+        href='/mypage/my-activities'
         className='group hover:text-primary mb-4 inline-flex items-center gap-2 rounded-lg px-4 py-3 text-base text-gray-600 hover:bg-gray-200'
       >
         <ArrowLeftIcon className='group-hover:text-primary h-4 w-4 text-gray-600' />
