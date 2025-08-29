3acc5fd9166f29b3b2ed2a15af11fd6753714415
htak0601 <taklee2@naver.com>
2025-08-29 16:55:32 +0900

    fix: 로그인 페이지 비밀번호 토글 버튼 태그 수정

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -137,7 +137,7 @@ const LoginPage = () => {
                 {...register('password')}
                 error={errors.password?.message}
               />
-              <button
+              <Button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
                 className="absolute right-4 top-15 -translate-y-1/2"
@@ -148,7 +148,7 @@ const LoginPage = () => {
                   width={24}
                   height={24}
                 />
-              </button>
+              </Button>
             </div>
 
             <div className='flex flex-col'>
