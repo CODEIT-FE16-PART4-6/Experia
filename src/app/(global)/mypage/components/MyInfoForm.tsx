98a76b6a327f77ea23973a45e432954f9f152157
김이서 <12305025k@gmail.com>
2025-08-29 13:42:19 +0900

    refactor: 인풋 스타일 상수 적용

--- src/app/(global)/mypage/components/MyInfoForm.tsx
+++ src/app/(global)/mypage/components/MyInfoForm.tsx
@@ -1,19 +1,20 @@
 import InputField from '@/components/InputField';
 
 const MyInfoForm = () => {
+  const INPUT_CLASS_NAME = 'mb-8 w-full';
   return (
     <div>
-      <InputField label='닉네임' placeholder='닉네임' className='mb-8'></InputField>
-      <InputField label='이메일' placeholder='이메일' className='mb-8'></InputField>
+      <InputField label='닉네임' placeholder='닉네임' className={INPUT_CLASS_NAME}></InputField>
+      <InputField label='이메일' placeholder='이메일' className={INPUT_CLASS_NAME}></InputField>
       <InputField
         label='비밀번호'
         placeholder='8자 이상 입력해 주세요'
-        className='mb-8'
+        className={INPUT_CLASS_NAME}
       ></InputField>
       <InputField
         label='비밀번호 재입력'
         placeholder='비밀번호를 한번 더 입력해 주세요'
-        className='mb-8'
+        className={INPUT_CLASS_NAME}
       ></InputField>
     </div>
   );
