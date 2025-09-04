3fb279880b07e6e7250db83dd26c68b6e43bc4b1
김이서 <12305025k@gmail.com>
2025-09-04 17:57:34 +0900

    hotfix: ReservateIn 컴포넌트 수정

--- src/app/(global)/activities/[id]/reservateIn.tsx
+++ src/app/(global)/activities/[id]/reservateIn.tsx
@@ -6,18 +6,12 @@ interface Props {
   data: ActivityDetail;
 }
 const ReservateIn = ({ data }: Props) => {
-  interface ReservateInProps {
-    activityId: number;
-  }
-
-  const ReservateIn = ({ activityId }: ReservateInProps) => {
-    return (
-      <>
-        <div className='hidden h-[423px] w-[251px] rounded-[10px] bg-[#b3b3b3] px-6 pt-6 pb-[18px] md:block lg:h-[746px] lg:w-[340px]'>
-          <Reservation data={data} />
-        </div>
-      </>
-    );
-  };
+  return (
+    <>
+      <div className='hidden h-[423px] w-[251px] rounded-[10px] bg-[#b3b3b3] px-6 pt-6 pb-[18px] md:block lg:h-[746px] lg:w-[340px]'>
+        <Reservation data={data} />
+      </div>
+    </>
+  );
 };
 export default ReservateIn;
