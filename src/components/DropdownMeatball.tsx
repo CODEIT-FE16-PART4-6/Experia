7afbed945498398d94687a1d419f6ba1f84087a9
Apologle <otl5994@naver.com>
2025-09-04 21:25:45 +0900

    [P6-126] style : 드롭다운 스타일 수정

--- src/components/DropdownMeatball.tsx
+++ src/components/DropdownMeatball.tsx
@@ -30,9 +30,9 @@ export const DropdownMeatball = ({ onEdit, onDelete }: DropdownMeatballProps) =>
 
       {/* 드롭다운 메뉴 */}
       {isOpen && (
-        <div className='absolute right-0 z-50 mt-2 w-40 rounded-md border border-gray-300 bg-white shadow-lg'>
+        <div className='group absolute top-10 right-2 z-50 mt-2 w-40 overflow-hidden rounded-md border border-gray-300 bg-white shadow-lg'>
           <div
-            className='h-[50px] w-full px-2 py-2 text-center'
+            className='flex h-[50px] w-full cursor-pointer flex-col justify-center border-b border-gray-300 px-2 py-2 text-center hover:bg-gray-200'
             onClick={() => {
               onEdit();
               setIsOpen(false);
@@ -41,7 +41,7 @@ export const DropdownMeatball = ({ onEdit, onDelete }: DropdownMeatballProps) =>
             수정하기
           </div>
           <div
-            className='h-[50px] w-full px-2 py-2 text-center'
+            className='group-hover:gray-100 flex h-[50px] w-full flex-col justify-center px-2 py-2 text-center hover:bg-gray-200'
             onClick={() => {
               onDelete();
               setIsOpen(false);
