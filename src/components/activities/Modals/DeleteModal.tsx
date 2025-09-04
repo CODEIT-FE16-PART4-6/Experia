03bd2588e091db57267edb0d5f80ce6d3af6022f
Apologle <otl5994@naver.com>
2025-09-05 03:22:42 +0900

    [P6-126] style : 모달 메세지 스타일링

--- src/components/activities/Modals/DeleteModal.tsx
+++ src/components/activities/Modals/DeleteModal.tsx
@@ -1,5 +1,5 @@
 import Button from '@/components/Button';
-
+import Image from 'next/image';
 interface titleType {
   title: string;
   onClose: () => void;
@@ -9,10 +9,20 @@ const DeleteModal = ({ title, onClose }: titleType) => {
   return (
     <div className='sm:fixed sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 md:static md:top-0 md:left-0 md:flex md:translate-0'>
       <div className='mt-[16px] flex w-100 flex-col justify-center gap-6 md:pl-6'>
-        <p className='text-center'>
-          <p className='font-bold'>{title}</p>
-          체험 포스터를 삭제하시겠습니까?
-        </p>
+        <div className='mr-[48px] flex justify-between'>
+          <div>
+            <Image
+              src='/icons/ActivityPageImgs/ic_CautionSign.svg'
+              alt='주의마크'
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className='self-center text-center'>
+            <p className='font-bold'>{title}</p>
+            체험 포스터를 <span className='text-[#df4040] underline'>삭제</span>하시겠습니까?
+          </div>
+        </div>
         <div className='flex gap-3'>
           <Button className='w-65'>예</Button>
           <Button className='w-65' onClick={onClose}>
