49ef4b65d7241de5a047787fc067d4ee8fece769
leesangdal <asksa1256@gmail.com>
2025-09-06 00:21:26 +0900

    [P6-135] chore: jest.config 불필요한 주석 제거

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Next.js 프로젝트의 루트 디렉터리를 Next.js에게 알려주기 위한 경로
   dir: './',
 });
 
-// Jest에 추가적인 설정을 적용하려면 아래와 같이 export를 수정하세요.
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
 };
 
-// Next.js Jest 설정을 비동기적으로 내보냅니다.
 module.exports = createJestConfig(customJestConfig);
