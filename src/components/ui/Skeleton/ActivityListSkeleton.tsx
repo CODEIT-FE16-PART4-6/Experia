5e9c39d8cae76d08add6c64ceb5ce45c4626359c
htak0601 <taklee2@naver.com>
2025-09-06 13:25:03 +0900

    feat: 스켈레톤 타이틀 제거

--- src/components/ui/Skeleton/ActivityListSkeleton.tsx
+++ src/components/ui/Skeleton/ActivityListSkeleton.tsx
@@ -1,6 +1,5 @@
 'use client';
 import ActivityItemSkeleton from './ActivityItemSkeleton';
-import SectionTitle from '../Section/SectionTitle';
 
 const ActivityListSkeleton = () => {
   const skeletonItems = Array.from({ length: 12 }, (_, index) => (
@@ -10,7 +9,6 @@ const ActivityListSkeleton = () => {
   return (
     <>
       <section className="mx-auto max-w-[1200px] mt-[34px] px-4">
-        <SectionTitle title="🌏 모든 체험" />
         <ul className="mb-12 grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
           {skeletonItems}
         </ul>
