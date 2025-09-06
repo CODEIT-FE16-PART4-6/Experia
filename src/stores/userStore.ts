b31f104e9ae41ec286cfac5ebfdbf1767ece2829
김이서 <12305025k@gmail.com>
2025-09-06 18:54:44 +0900

    [P6-132] refactor: 내정보 수정 기능, zustand에 저장기능

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -7,6 +7,7 @@ import { User } from '@/types/schema/userSchema';
 export interface UserState {
   user: User | null;
   setUser: (user: User | null) => void;
+  updateUser: (updates: Partial<User>) => void;
   clearUser: () => void;
 }
 
@@ -18,6 +19,12 @@ export const useUserStore = create<UserState>()(
 
       // 액션: 로그인
       setUser: user => set({ user }),
+
+      // 액션: 사용자 정보 업데이트
+      updateUser: updates =>
+        set(state => ({
+          user: state.user ? { ...state.user, ...updates } : null,
+        })),
 
       // 액션: 로그아웃
       clearUser: () => set({ user: null }),
