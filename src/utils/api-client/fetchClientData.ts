92a2174da71bef12efbdb15b484d14a0f9ed0636
leesangdal <asksa1256@gmail.com>
2025-09-09 18:11:39 +0900

    [P6-142] fix: fetchClientData - accessToken 변수 const → let으로 변경

--- src/utils/api-client/fetchClientData.ts
+++ src/utils/api-client/fetchClientData.ts
@@ -45,7 +45,7 @@ const refreshAccessToken = async () => {
 
 // fetch 래퍼 함수 (외부에서 이 함수 사용)
 const fetchClientData = async (endpoint: string, options: RequestInit = {}) => {
-  const accessToken = localStorage.getItem('access_token');
+  let accessToken = localStorage.getItem('access_token');
   const headers = new Headers(options.headers);
 
   // 요청 헤더에 액세스 토큰 추가
