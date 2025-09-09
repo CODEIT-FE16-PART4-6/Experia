2507b54da443daff71daf726be539b8ed7bc7526
htak0601 <taklee2@naver.com>
2025-09-09 12:58:48 +0900

    feat: PopularPageClient에 PopularList 연결 (초기 데이터)

--- src/components/activities/PopularPage.client.tsx
+++ src/components/activities/PopularPage.client.tsx
@@ -4,6 +4,7 @@ import { BREAKPOINTS, POPULAR_ACTIVITIES_VIEW_COUNT } from "@/constants"
 import useWindowWidth from "@/hooks/useWindowWidth"
 import { Activities } from "@/types/schema/activitiesSchema"
 import { useEffect, useState } from "react"
+import PopularList from "./PopularList.client"
 
 const getPageSize = (width: number) => {
   if (width >= BREAKPOINTS.lg) return POPULAR_ACTIVITIES_VIEW_COUNT.lg
@@ -19,7 +20,11 @@ const PopularPageClient = ({ initialData }: { initialData: Activities }) => {
     if (innerWidth) setPageSize(getPageSize(innerWidth))
   }, [innerWidth])
 
-  return <div className="pl-4 lg:px-0 mb-10 md:mb-14 lg:mb-20">{/* 리스트 추가 예정 */}</div>
+  return (
+    <div className="pl-4 lg:px-0 mb-10 md:mb-14 lg:mb-20">
+      <PopularList data={{ pages: [initialData], pageParams: [null] }} />
+    </div>
+  )
 }
 
 export default PopularPageClient
