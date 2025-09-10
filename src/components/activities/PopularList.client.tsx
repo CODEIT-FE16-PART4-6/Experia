2f34f94c8a9934b85adcd61eb0581e7dfe1e7001
htak0601 <taklee2@naver.com>
2025-09-10 16:38:54 +0900

    feat: 인기 체험 섹션 Swiper 기능 추가

--- src/components/activities/PopularList.client.tsx
+++ src/components/activities/PopularList.client.tsx
@@ -1,23 +1,54 @@
 'use client';
 
-import { Activity, PopularActivities } from '@/types/schema/activitiesSchema'
-import PopularItem from './PopularItem'
+import { PopularActivities } from '@/types/schema/activitiesSchema'
 import { InfiniteData } from '@tanstack/react-query';
+import PopularItem from './PopularItem';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import LoadingSpinner from '../ui/LoadingSpinner';
+import { BREAKPOINTS } from '@/constants';
 
 interface PopularListProps {
   data: InfiniteData<PopularActivities, string | null>;
+  fetchNextPage: () => void;
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
 }
 
-
-const PopularList = ({ data }: PopularListProps) => {
+const PopularList = ({ data, fetchNextPage, hasNextPage, isFetchingNextPage }: PopularListProps) => {
+  const isFetchingMore = hasNextPage && isFetchingNextPage;
   return (
-    <ul className="grid grid-flow-col gap-4 overflow-x-auto auto-cols-[25%]">
-      {data.pages.map(page =>
-        page.activities.map(activity => (
-          <PopularItem key={activity.id} item={activity} />
-        ))
-      )}
-    </ul>
+    <div className="relative">
+      <Swiper
+        slidesPerView={1} // 모바일 기본
+        spaceBetween={16} // 슬라이드 간격
+        breakpoints={{
+          [BREAKPOINTS.md]: { slidesPerView: 2 }, // 720px 이상
+          [BREAKPOINTS.lg]: { slidesPerView: 3 }, // 1280px 이상
+        }}
+        navigation  // 좌우 버튼
+        grabCursor  // 마우스 드래그 가능
+        modules={[Navigation]}  // Swiper 모듈 등록
+        onReachEnd={() => {
+          if (hasNextPage) fetchNextPage()
+        }}
+      >
+        {data.pages.map(page =>
+          page.activities.map(activity => (
+            <SwiperSlide key={activity.id}>
+              <PopularItem item={activity} />
+            </SwiperSlide>
+          ))
+        )}
+        {isFetchingMore && (
+          <SwiperSlide className="w-auto flex items-center justify-center">
+            <LoadingSpinner />
+          </SwiperSlide>
+        )}
+      </Swiper>
+    </div>
   );
 };
 
