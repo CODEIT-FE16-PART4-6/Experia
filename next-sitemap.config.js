9e96375cd33f624ce109dfec85a71bfb12fd9cbc
asksa1256@gmail.com <asksa1256@gmail.com>
2025-09-13 21:04:28 +0900

    feat: 사이트맵 설정 수정 (마이페이지 생성 제외)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteUrl: 'https://experia.click', // 배포된 도메인
   generateRobotsTxt: true, // robots.txt 자동 생성
-  exclude: ['/mypage/*'], // 크롤링 제외
+  exclude: ['/mypage', '/mypage/*'], // 크롤링 제외
 };
