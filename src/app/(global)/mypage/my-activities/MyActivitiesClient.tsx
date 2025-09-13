fe27185f8f6ac9df5cd79f9a872282fc856160e8
leesangdal <asksa1256@gmail.com>
2025-09-13 16:19:08 +0900

    feat: 내 체험 관리 무한스크롤 적용

---
+++ src/app/(global)/mypage/my-activities/MyActivitiesClient.tsx
@@ -0,0 +1,127 @@
+'use client';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useRef } from 'react';
+
+import ActivityCard from '@/app/(global)/mypage/components/ActivityCard';
+import { LinkButton } from '@/components/ui/LinkButton';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
+import SectionTitle from '@/components/ui/Section/SectionTitle';
+import { MYPAGE_CARDS_COUNT } from '@/constants';
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+import { Activities, ActivityType } from '@/types/schema/activitiesSchema';
+import fetchClientData from '@/utils/api-client/fetchClientData';
+
+interface fetchMyActivitiesProps {
+  cursorId: number | null;
+}
+
+const fetchMyActivities = async ({ cursorId }: fetchMyActivitiesProps) => {
+  const params = new URLSearchParams();
+  params.set('size', String(MYPAGE_CARDS_COUNT));
+
+  if (cursorId !== null) params.set('cursorId', cursorId.toString());
+
+  const response = await fetchClientData(`/my-activities?${params.toString()}`);
+  const validatedData = Activities.parse(response);
+
+  return validatedData;
+};
+
+const MyActivitiesClient = () => {
+  const loadMoreRef = useRef<HTMLDivElement>(null);
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, error, isPending, refetch } =
+    useInfiniteQuery({
+      queryKey: ['myActivities'],
+      queryFn: ({ pageParam }) => {
+        return fetchMyActivities({
+          cursorId: pageParam ?? null,
+        });
+      },
+      initialPageParam: null as number | null,
+      getNextPageParam: lastPage => lastPage.cursorId ?? undefined,
+    });
+
+  const allMyActivities = data?.pages.flatMap(page => page.activities) ?? [];
+  const isFetchingMore = hasNextPage && isFetchingNextPage;
+
+  const handleDeleteSuccess = () => {
+    refetch(); // 삭제 성공 시 데이터 다시 불러오기
+  };
+
+  useIntersectionObserver({
+    target: loadMoreRef,
+    onIntersect: fetchNextPage,
+    enabled: hasNextPage,
+  });
+
+  return (
+    <div className='min-h-screen'>
+      <SectionTitle
+        title='내 체험 관리'
+        action={<LinkButton href='/mypage/my-activities/add-activity'>체험 등록하기</LinkButton>}
+      />
+
+      <div className='flex flex-col gap-6 md:gap-4'>
+        {isPending && (
+          <div className='flex flex-col items-center justify-center gap-2 text-gray-500'>
+            <LoadingSpinner /> 목록을 불러오는 중입니다...
+          </div>
+        )}
+
+        {!isPending &&
+          allMyActivities.map(activity => (
+            <ActivityCard
+              key={activity.id}
+              type='activity'
+              data={activity as ActivityType}
+              onDeleteSuccess={handleDeleteSuccess}
+            />
+          ))}
+
+        {!isPending && allMyActivities.length === 0 && (
+          <p className='py-8 text-center text-gray-500'>등록하신 체험이 없습니다.</p>
+        )}
+      </div>
+
+      {/* <div className='flex flex-col gap-6 md:gap-4'>
+        {isPending && (
+          <div className='flex flex-col items-center justify-center gap-2 text-gray-500'>
+            <LoadingSpinner /> 목록을 불러오는 중입니다...
+          </div>
+        )}
+
+        {!isPending &&
+          filteredReservations.map(reservation => (
+            <ActivityCard
+              key={reservation.id}
+              type='reservation'
+              data={reservation as ReservationType}
+            />
+          ))}
+
+        {!isPending && filteredReservations.length === 0 && (
+          <div className='py-8 text-center text-gray-500'>
+            {selectedStatus ? '선택한 필터에 해당하는 예약이 없습니다.' : '예약 내역이 없습니다.'}
+          </div>
+        )}
+      </div> */}
+
+      <div ref={loadMoreRef} className='min-h-10'>
+        {error && (
+          <p className='pb-16 text-center'>
+            목록 불러오기에 실패했습니다.
+            <button className='ml-2 underline underline-offset-4' onClick={() => fetchNextPage()}>
+              다시 시도
+            </button>
+          </p>
+        )}
+
+        {isFetchingMore && <LoadingSpinner />}
+      </div>
+    </div>
+  );
+};
+
+export default MyActivitiesClient;
--- src/app/(global)/mypage/my-activities/page.tsx
+++ src/app/(global)/mypage/my-activities/page.tsx
@@ -1,52 +1,64 @@
-'use client';
-import { useQuery } from '@tanstack/react-query';
+// 'use client';
+// import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 
-import ActivityCard from '@/app/(global)/mypage/components/ActivityCard';
-import { LinkButton } from '@/components/ui/LinkButton';
-import SectionTitle from '@/components/ui/Section/SectionTitle';
-import { Activities, ActivityType } from '@/types/schema/activitiesSchema';
-import fetchClientData from '@/utils/api-client/fetchClientData';
+// import ActivityCard from '@/app/(global)/mypage/components/ActivityCard';
+// import { LinkButton } from '@/components/ui/LinkButton';
+// import SectionTitle from '@/components/ui/Section/SectionTitle';
+// import { Activities, ActivityType } from '@/types/schema/activitiesSchema';
+// import fetchClientData from '@/utils/api-client/fetchClientData';
 
-const fetchMyActivities = async () => {
-  const response = await fetchClientData('/my-activities?size=20');
-  const validatedData = Activities.parse(response);
-  return validatedData.activities;
-};
+// const fetchMyActivities = async () => {
+//   const response = await fetchClientData('/my-activities?size=20');
+//   const validatedData = Activities.parse(response);
+//   return validatedData.activities;
+// };
+
+// const MyActivitiesPage = () => {
+//   const { data, error, isLoading, refetch } = useQuery({
+//     queryKey: ['activityList'],
+//     queryFn: fetchMyActivities,
+//   });
+
+//   const handleDeleteSuccess = () => {
+//     refetch(); // 삭제 성공 시 데이터 다시 불러오기
+//   };
+
+//   if (isLoading) return <div>로딩중...</div>;
+//   if (error) return <div>오류 발생: {error.message}</div>;
+
+//   const activities = data || [];
+
+//   return (
+//     <div>
+//   <SectionTitle
+//     title='내 체험 관리'
+//     action={<LinkButton href='/mypage/my-activities/add-activity'>체험 등록하기</LinkButton>}
+//   />
+
+//   <div className='flex flex-col gap-6 md:gap-4'>
+//     {activities.map(activity => (
+//       <ActivityCard
+//         key={activity.id}
+//         type='activity'
+//         data={activity as ActivityType}
+//         onDeleteSuccess={handleDeleteSuccess}
+//       />
+//     ))}
+//   </div>
+// </div>
+//   );
+// };
+
+// export default MyActivitiesPage;
+import { Suspense } from 'react';
+
+import MyActivitiesClient from './MyActivitiesClient';
 
 const MyActivitiesPage = () => {
-  const { data, error, isLoading, refetch } = useQuery({
-    queryKey: ['activityList'],
-    queryFn: fetchMyActivities,
-  });
-
-  const handleDeleteSuccess = () => {
-    refetch(); // 삭제 성공 시 데이터 다시 불러오기
-  };
-
-  if (isLoading) return <div>로딩중...</div>;
-  if (error) return <div>오류 발생: {error.message}</div>;
-
-  const activities = data || [];
-
   return (
-    <div>
-      <SectionTitle
-        title='내 체험 관리'
-        action={<LinkButton href='/mypage/my-activities/add-activity'>체험 등록하기</LinkButton>}
-      />
-
-      <div className='flex flex-col gap-6 md:gap-4'>
-        {activities.map(activity => (
-          <ActivityCard
-            key={activity.id}
-            type='activity'
-            data={activity as ActivityType}
-            onDeleteSuccess={handleDeleteSuccess}
-          />
-        ))}
-      </div>
-    </div>
+    <Suspense fallback={<div>로딩중...</div>}>
+      <MyActivitiesClient />
+    </Suspense>
   );
 };
-
 export default MyActivitiesPage;
